81075767302d4edf5f2b15ee1917bb98638787f9
Adem Pelit <Adem Pelit@DESKTOP-07EG7C1>
2022-09-20 18:49:45 +0300

    Proje dosyası ekle.

---
+++ Prime Numbers/PrimeNumberGenerator.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+public class PrimeNumberGenerator
+{
+    public List<int> PrimeNumbers { get { return _primeNumbers; } }
+
+    private List<int> _primeNumbers;
+    private int _limit;
+
+    public PrimeNumberGenerator(int limit)
+    {
+        this._limit = limit;
+        _primeNumbers = new List<int>() { };
+    }
+
+    internal void Generate()
+    {
+        for (int number = 2; number <= _limit; number++)
+        {
+            if (isPrime(number)) _primeNumbers.Add(number);
+        }
+        _primeNumbers.Insert(0, 1);
+    }
+
+    private bool isPrime(int number)
+    {
+        var halfOfNumber = number / 2;
+        foreach (var primeNumber in _primeNumbers)
+        {
+            if (primeNumber > halfOfNumber) break;
+            if (isDivisible(number, primeNumber)) return false;
+        }
+        return true;
+    }
+
+    private bool isDivisible(int number, int divider) => number % divider == 0;
+}
---
+++ Prime Numbers/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Prime_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int limit = 100;
+            var generator = new PrimeNumberGenerator(limit);
+            generator.Generate();
+
+            Console.WriteLine(limit + " değerine kadar olan asal sayılar:");
+            foreach (var primeNumber in generator.PrimeNumbers)
+                Console.WriteLine(primeNumber);
+        }
+    }
+}
